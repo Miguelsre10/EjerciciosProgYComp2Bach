4cafd64aa45bf32da589e90994361cb977862deb
user10 <user10@192.168.0.112>
2021-10-11 13:11:46 +0200

    Tarea 1 entrega 21/10/13

---
+++ src/main/java/Capitulo01/Ejercicio01.java
@@ -0,0 +1,22 @@
+package Capitulo01;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Introduzca un numero entero:");
+		int numEntero = sc.nextInt();
+
+		System.out.println("Introduzca un numero flotrate");
+		float numFlotante = sc.nextFloat();
+
+		System.out.println("Introduzca un numero doble");
+		double numDoble = sc.nextDouble();
+		
+		System.out.println("El numero entero es: "+ numEntero + ". El numero flotante es: "+ numFlotante+ ", El numero doble es: " + numDoble ); 
+	}
+
+}
