74480feb8f091510972b9f08b62ca6a6d07c54c8
Miguelsre10 <miguelsre10@gmail.com>
2021-10-26 10:30:59 +0200

    26/10

--- src/main/java/Capitulo01/Ejercicio02_CalculoCuotaMensualPrestamo.java
+++ src/main/java/Capitulo01/Ejercicio02_CalculoCuotaMensualPrestamo.java
@@ -7,9 +7,8 @@ public class Ejercicio02_CalculoCuotaMensualPrestamo {
 	public static void main(String[] args) {
 		int meses;
 		float capital, euribor;
-		float Diferencial;
-		float interesAnual, interesMensual, auxilar, cuotaMensual;
-		
+		float diferencial, Auxiliar;
+		float interesAnual, interesMensual, cuotaMensual;		
 
 		
 		Scanner sc = new Scanner(System.in) ;
@@ -17,24 +16,25 @@ public class Ejercicio02_CalculoCuotaMensualPrestamo {
 		capital = sc.nextFloat();
 		System.out.println("El capital es: "+ capital);
 		
+		System.out.println("Introduzca el numero de meses: ");
+		meses = sc.nextInt();
+		System.out.println("El numero de meses es: ");
+		
 		System.out.println("Introduzca el euribor: ");
 		euribor = sc.nextFloat();
 		System.out.println("El euribor es: "+ euribor);
 		
 		System.out.println("Introduzca el diferencial: ");
-		capital = sc.nextFloat();
-		System.out.println("El diferencial es: "+ Diferencial );
+		diferencial = sc.nextFloat();
+		System.out.println("El diferencial es: "+ diferencial );
 		
-		System.out.println("Introduzca el capital: ");
-		meses = sc.nextInt();
-		System.out.println("El numero de meses es: ");
 		
 // comenzamos los calculos		
 		
-		interesAnual = euribor + Diferencial ;
+		interesAnual = euribor + diferencial ;
 		interesMensual = interesAnual / 12 /100;
-		auxiliar =(float) Math.pow(1 + interesMensual, meses);
-		cuotaMensual = (interesMensual*auxiliar) / (auxiliar - 1);
+		Auxiliar =(float) Math.pow(1 + interesMensual, meses);
+		cuotaMensual = capital * (interesMensual*Auxiliar) / (Auxiliar - 1);
 		System.out.println("debes pagar " + cuotaMensual + "euros al mes");
 		
 		
