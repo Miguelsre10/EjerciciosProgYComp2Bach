3769dd28776b220664e748f39fb3618703649793
Miguelsre10 <miguelsre10@gmail.com>
2022-03-25 11:29:55 +0100

    conx2 serv

--- src/main/java/Capitulo06_AccesoADatos/Ejemplo01_ConexionBasica.java
+++ src/main/java/Capitulo06_AccesoADatos/Ejemplo01_ConexionBasica.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Ejemplo01_ConexionBasica {
 
@@ -52,13 +53,126 @@ public class Ejemplo01_ConexionBasica {
 		}
 	}
 	
+	/**
+	 * 	
+	 */
+	
+	private static void pruebaUpdate () {
+		int id;
+		String nuevoNombre;
+		String nuevoApellido;
+		String nuevoNIF;
+		Scanner sc = new Scanner (System.in);
+		
+		try {
+			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
+			// no podemos trabajar con esa BBDD.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		   
+			// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de tipo Connection, al cual
+			// le tenemos que pasar los parámetros de conexión.
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+		   
+			
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			//le pedimos unos datos
+			System.out.println("intruduce el id del registro afectado: ");
+			id = Integer.parseInt(sc.next());
+			System.out.println("intruduce el nuevo nombre del registro: ");
+			nuevoNombre = sc.next();
+			System.out.println("intruduce el nuevo apellido del registro: ");
+			nuevoApellido = sc.next();
+			System.out.println("intruduce el nuevo nif del registro: ");
+			nuevoNIF = sc.next();
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			int numeroRegistroAfectados = s.executeUpdate ( 
+					"UPDATE alumnos.alumno set nombre='" + nuevoNombre + " ' , apellidos= '"+ nuevoApellido + " ' , nif = '"+ nuevoNIF + "' where id="+ id + ";");
+		   
+			System.out.println(numeroRegistroAfectados + " registros afectados");
+			
+			
+			// Cierre de los elementos
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	/**
+	 * 
+	 */
+	
+	private static void pruebaInsert () {
+		int nuevoid;
+		String nuevoNombre;
+		String nuevoApellido;
+		String nuevoNIF;
+		Scanner sc = new Scanner (System.in);
+		
+		try {
+			// A través de la siguiente línea comprobamos si tenemos acceso al driver MySQL, si no fuera así
+			// no podemos trabajar con esa BBDD.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		   
+			// Necesitamos obtener un acceso a la BBDD, eso se materializa en un objeto de tipo Connection, al cual
+			// le tenemos que pasar los parámetros de conexión.
+			Connection conexion = (Connection) DriverManager.getConnection ("jdbc:mysql://localhost/alumnos?serverTimezone=UTC","root", "Abcdefgh.1");
+		   
+			
+			// Para poder ejecutar una consulta necesitamos utilizar un objeto de tipo Statement
+			Statement s = (Statement) conexion.createStatement(); 
+			
+			//le pedimos unos datos
+			System.out.println("intruduce el id del nuevo registro : ");
+			nuevoid = Integer.parseInt(sc.next());
+			System.out.println("intruduce el nuevo nombre del registro: ");
+			nuevoNombre = sc.next();
+			System.out.println("intruduce el nuevo apellido del registro: ");
+			nuevoApellido = sc.next();
+			System.out.println("intruduce el nuevo nif del registro: ");
+			nuevoNIF = sc.next();
+			
+			// La ejecución de la consulta se realiza a través del objeto Statement y se recibe en forma de objeto
+			// de tipo ResultSet, que puede ser navegado para descubrir todos los registros obtenidos por la consulta
+			int numeroRegistroAfectados = s.executeUpdate ( 
+					"insert into alumnos.alumno VALUES (" + nuevoid +" , '" + nuevoNombre + "' ,'"+ nuevoApellido + "', '"+ nuevoNIF + "')");
+		   
+			System.out.println(numeroRegistroAfectados + " registros afectados");
+			
+			
+			// Cierre de los elementos
+			s.close();
+			conexion.close();
+		}
+		catch (ClassNotFoundException ex) {
+			System.out.println("Imposible acceder al driver Mysql");
+			ex.printStackTrace();
+		}
+		catch (SQLException ex) {
+			System.out.println("Error en la ejecución SQL: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+	
 	
 	/**
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		pruebaConsultaBasica();
+		pruebaInsert();
 	}
 
 }
+
+
